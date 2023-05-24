c14a01b06257f42ce1c7e57d5139048b821d5ea4
john <aarontu803@gmail.com>
2023-05-24 17:20:05 +0800

    add partnerComp

---
+++ src/components/PartnerComp.vue
@@ -0,0 +1,57 @@
+<script>
+export default {
+  data() {
+    return {
+      partnerList: [
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise1.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise2.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise3.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise4.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise5.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise6.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise7.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise8.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise9.png',
+        '../../public/Rectangle22.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise11.png',
+        'https://raw.githubusercontent.com/hexschool/2022-web-layout-training/main/2023web-camp/enterprise12.png'
+      ]
+    }
+  }
+}
+</script>
+<template>
+  <div>
+    <h2 class="mb-6 mb-lg-20 fs-1 fw-bold">來自合作夥伴</h2>
+    <div class="overflow-hidden h40 gap-5 mb-5">
+      <ul class="d-flex justify-content-xl-center partner-list mask pb-5 px-10">
+        <li class="" v-for="(img, index) in partnerList.slice(0, 6)" :key="index + img">
+          <img class="img" :src="img" :alt="`partner${index}`" />
+        </li>
+      </ul>
+    </div>
+    <div class="overflow-hidden h40 gap-5">
+      <ul
+        class="d-flex justify-content-xl-center partner-list mask pb-5 position-relative ps-2 pe-8"
+      >
+        <li class="" v-for="(img, index) in partnerList.slice(6, 12)" :key="index + img">
+          <img class="img" :src="img" :alt="`partner${index}`" />
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
+<style>
+.h40 {
+  height: 40px;
+}
+.partner-list {
+  overflow-x: scroll;
+  mask: linear-gradient(90deg, #000 80%, transparent);
+  -webkit-mask: linear-gradient(90deg, transparent 2%, #000 10%, #000 70%, transparent);
+  gap: 0px 1.25rem;
+}
+.img {
+  max-width: 160px;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,19 @@
-<script >
+<script>
 import HeaderComp from '../components/HeaderComp.vue'
 import FooterComp from '../components/FooterComp.vue'
 import Banner from '../components/HomeBanner.vue'
 import Feature from '../components/FeatureComp.vue'
 import GetStart from '../components/GetStart.vue'
+import Partner from '../components/PartnerComp.vue'
 export default {
   components: {
     HeaderComp,
     Banner,
     FooterComp,
     Feature,
-    GetStart
-  }
+    GetStart,
+    Partner
+  },
 }
 </script>
 
@@ -19,12 +21,11 @@ export default {
   <div class="container">
     <HeaderComp></HeaderComp>
     <Banner></Banner>
-    <Feature></Feature>
+    <Feature class="mb-30 mb-lg-40"></Feature>
+    <Partner class="mb-12 mb-lg-40"></Partner>
     <GetStart></GetStart>
     <FooterComp></FooterComp>
   </div>
-
-    
 </template>
 
 <style>
