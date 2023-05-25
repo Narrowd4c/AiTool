5d16a6ce8e951db889082909b5baffbfba2e91fb
john <aarontu803@gmail.com>
2023-05-25 13:20:41 +0800

    githubpage config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:'/AiTool',
+  base:'/AiTool/',
   plugins: [vue()],
   resolve: {
     alias: {
