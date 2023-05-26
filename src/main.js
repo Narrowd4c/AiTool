1c976a9ff75b38022e971b42a09b80a4529fb6ba
john <aarontu803@gmail.com>
2023-05-26 14:01:06 +0800

    add PriceComp

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
-console.log('it just work')
 app.use(router)
 
 app.mount('#app')
---
+++ src/views/PriceView.vue
@@ -0,0 +1,31 @@
+<script>
+import PriceBanner from '../components/PriceBanner.vue'
+import PriceComp from '../components/PriceComp.vue'
+import RulesComp from '../components/RulesComp.vue'
+import AiTool from '../components/AiTool.vue'
+import FaqComp  from '../components/FaqComp.vue'
+export default {
+    components: {
+        PriceBanner,
+        PriceComp,
+        RulesComp,
+        AiTool,
+        FaqComp,
+    }
+}
+</script>
+<template>
+    <div class="container">
+        <PriceBanner></PriceBanner>
+        <PriceComp></PriceComp>
+        <RulesComp></RulesComp>
+    </div>
+    <AiTool class="mb-30 mb-lg-40"></AiTool>
+    <div class="container">
+        <FaqComp class="mb-30 mb-lg-40"></FaqComp>
+    </div>
+</template>
+<style>
+
+</style>
+
