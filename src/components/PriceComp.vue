b01cac4be2543810b2f569902bc4702ca8ddb4d5
john <aarontu803@gmail.com>
2023-05-26 14:01:06 +0800

    add PriceComp

---
+++ src/components/PriceComp.vue
@@ -0,0 +1,53 @@
+<script>
+export default{ 
+    data() {
+        return {
+            priceList: [
+                {
+                    name: '琪琪',
+                    description: ['初級', '速度最快'],
+                    price: '5',
+                },
+                {
+                    name: '昊昊',
+                    description: ['中級'],
+                    price: '5',
+                },
+                {
+                    name: '卡卡',
+                    description: ['高級'],
+                    price: '5',
+                },
+                {
+                    name: '杰杰',
+                    description: ['最高級', '資訊最完善'],
+                    price: '5',
+                }
+            ],
+        }
+    }
+}
+</script>
+<template>
+    <h3 class="fz-8 mb-20 fw-bold">定價</h3>
+  <ul class="row gap-y-6 mb-30">
+    <li v-for="({name, description, price}, i) in priceList" :key="i+name" class="col-md-6">
+      <div class="border rounded-4 h-100 vstack">
+        <div class='p-5 px-lg-8 py-lg-5 flex-grow-1'>
+        <h4 class="fw-bold fz-8 mb-3">{{ name }}</h4>
+        <ul class="list-style ms-5 mb-10 flex-grow-1">
+          <li v-for="d in description" :key="d">{{ d }}</li>
+        </ul>
+        </div>
+        <div class="border-top hstack p-5">
+          <p class="fz-5 fw-bold">NT${{ price }}<span class="text-gray">／1k tokens</span></p>
+          <a class="cursor-pointer ms-auto fw-bolder">開始使用<span class="fs-sub align-text-bottom material-symbols-outlined">
+north_east
+</span></a>
+        </div>
+      </div>
+    </li>
+  </ul>
+</template>
+<style>
+</style>
---
+++ src/components/RulesComp.vue
@@ -0,0 +1,24 @@
+<script>
+export default {
+  data() {
+      return {
+          rules: [
+              { title: '守法', description: '請遵守相關的法規和政策，不得進行任何違法違規的活動。' },
+              { title: '資訊安全', description: '請務必保護好自己的帳號和密碼，不得將其透露給他人。' },
+              { title: '守密', description: '需對使用過程中產生的數據負責，不得擅自泄露數據。' },
+          ]
+    }
+  }
+}
+</script>
+<template>
+    <div class="d-lg-flex justify-content-between mb-30">
+    <h3 class="fw-bold">使用規範</h3>
+    <ul>
+        <li class="py-10 border-bottom border-secondary" v-for="({title, description}) in rules" :key="title" >
+            <h4 class="mb-6 fz-8 fw-bold">{{ title }}</h4>
+            <p class="">{{ description }}</p>
+        </li>
+    </ul>
+  </div>
+</template>
