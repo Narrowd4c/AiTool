4745c36b25ec30cbf1864a70529933b93f675cf1
john <aarontu803@gmail.com>
2023-05-28 16:27:58 +0800

    change FaqComp answer

--- src/components/FaqComp.vue
+++ src/components/FaqComp.vue
@@ -3,11 +3,11 @@ export default {
   data() {
     return {
       faqList: [
-        { question: '如何選擇適合的AI模型？', answer: '符合你需求的AI模型', hidden: false },
-        { question: '租用模型的費用是如何計算的？', answer: '用加法', hidden: false },
-        { question: '如何進行付款?', answer: '....', hidden: false },
-        { question: '租用模型的期限是多久？', answer: '.....', hidden: false },
-        { question: '如果在使用過程中遇到問題，應該怎麼處理？', answer: '...', hidden: false }
+        { question: '如何選擇適合的AI模型？', answer: '您可以根據應用需求和資料特性，評估模型的性能、準確性和適應性，以選擇最適合的AI模型。', hidden: false },
+        { question: '租用模型的費用是如何計算的？', answer: '租用模型的費用是根據使用時間、計算資源和服務等級來計算的。', hidden: false },
+        { question: '如何進行付款?', answer: '您可以透過我們的網站，使用安全的付款通道完成付款。', hidden: false },
+        { question: '租用模型的期限是多久？', answer: '租用模型的期限根據您選擇的租用計劃而定，可以是每日或自訂的時間範圍。', hidden: false },
+        { question: '如果在使用過程中遇到問題，應該怎麼處理？', answer: '如果在使用過程中遇到問題，請聯繫我們的客服團隊，我們將提供技術支援和解決方案。', hidden: false }
       ]
     }
   },
